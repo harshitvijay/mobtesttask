90543c0f58c9f223fa3d959a0fd3f13236c3749e
hviaywargiya <hvijaywargiya@deqode.com>
2022-02-15 18:13:24 +0530

    Added Landing Page

---
+++ src/Container/LandingPage/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import Header from "../../Components/Header";
+
+const LandingPage: FC = () => {
+  return <Header title="Test Task" />;
+};
+
+export default LandingPage;
