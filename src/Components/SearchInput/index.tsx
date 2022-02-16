d71ab596e6b58b3cf2261d8f41f23f32b00d5d06
hviaywargiya <hvijaywargiya@deqode.com>
2022-02-16 17:20:55 +0530

    Updated Input Search Component

---
+++ src/Components/SearchInput/index.tsx
@@ -0,0 +1,37 @@
+import { FC, useState, useContext } from "react";
+import Container from "@mui/material/Container";
+import Input from "@mui/material/Input";
+import useStyles from "./style";
+import UserCardList from "../UserCardList";
+import { StateContext, StateStore } from "../../Mobx/store";
+
+const SearchInput: FC = () => {
+  const classes = useStyles();
+  const [query, setQuery] = useState("");
+  const state: StateStore = useContext(StateContext);
+  const data = state.users;
+
+  return (
+    <>
+      <Container className={classes.searchContainer}>
+        <Input
+          placeholder="Search Monster"
+          className={classes.searchStyle}
+          onChange={(event) => setQuery(event.target.value)}
+        />
+      </Container>
+      <UserCardList
+        // eslint-disable-next-line array-callback-return
+        data={data.filter((user) => {
+          if (query === "") {
+            return user;
+          } else if (user.name.toLowerCase().includes(query.toLowerCase())) {
+            return user;
+          }
+        })}
+      />
+    </>
+  );
+};
+
+export default SearchInput;
---
+++ src/Components/SearchInput/style.ts
@@ -0,0 +1,19 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  searchContainer: {
+    display: "flex !important",
+    justifyContent: "center",
+    marginTop: "2%",
+    height: "50px",
+  },
+  searchStyle: {
+    backgroundColor: "white",
+    width: "40%",
+    paddingLeft: "2%",
+    borderRadius: "2%",
+    border: "1px solid white",
+  },
+});
+
+export default useStyles;
