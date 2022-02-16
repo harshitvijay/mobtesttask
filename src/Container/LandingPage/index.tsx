17e7215cead03ee7f1753b453d59da0aacbf1812
hviaywargiya <hvijaywargiya@deqode.com>
2022-02-16 17:21:41 +0530

    Updated Landing Page

--- src/Container/LandingPage/index.tsx
+++ src/Container/LandingPage/index.tsx
@@ -1,8 +1,14 @@
 import { FC } from "react";
 import Header from "../../Components/Header";
+import SearchInput from "../../Components/SearchInput";
 
 const LandingPage: FC = () => {
-  return <Header title="Test Task" />;
+  return (
+    <>
+      <Header title="Test Task" />
+      <SearchInput />
+    </>
+  );
 };
 
 export default LandingPage;
