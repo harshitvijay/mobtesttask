a0891db65f882c3c4e673600d1df9159cad7dd8b
hviaywargiya <hvijaywargiya@deqode.com>
2022-02-16 17:19:51 +0530

    Updated App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import { FC } from "react";
+import { FC, useContext } from "react";
 import LandingPage from "./Container/LandingPage";
+import { StateContext, StateStore } from "./Mobx/store";
+import { observer } from "mobx-react-lite";
+import { fetchUsers } from "./service";
+import { url } from "./constants";
 import "./App.css";
 
-const App: FC = () => {
+const App: FC = observer(() => {
+  const state: StateStore = useContext(StateContext);
+  fetchUsers(url).then((users) => (state.users = users));
   return <LandingPage />;
-};
+});
 
 export default App;
