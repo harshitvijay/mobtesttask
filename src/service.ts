b1664a2c229dabfa6f0946ed5e02e5171ab6ad51
hviaywargiya <hvijaywargiya@deqode.com>
2022-02-16 17:20:32 +0530

    Data Fetch From API

---
+++ src/service.ts
@@ -0,0 +1,10 @@
+// import { StateStore } from "./Mobx/store";
+export const fetchUsers = async (url: string) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
